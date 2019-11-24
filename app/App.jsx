293aebc6204c2ec3ba29efe45395c089227337e2
unknown <josiahjswab@gmail.com>
2019-11-23 22:11:37 -0800

    Prunes files deemed unneeded as app has matured. git push origin about-us

--- app/App.jsx
+++ app/App.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-import importedComponent from "react-imported-component";
 import { Helmet } from "react-helmet";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HelloWorld from "./HelloWorld";
 import Flexstart from "./components/Flexstart";
+import AboutUs from "./components/AboutUs";
 
 import "./css/styles.scss";
-
-const HelloWorld2 = importedComponent(() => import("./HelloWorld2"));
 
 export default class App extends Component {
   constructor(props) {
@@ -36,8 +33,8 @@ export default class App extends Component {
           <Header display={this.state.display}/>
           <div style={{ display: this.state.display }} className="spacer-header"></div>
           <Switch>
-            <Route exact path="/" component={HelloWorld} />
-            <Route path="/codeSplit" component={HelloWorld2} />
+            <Route exact path="/" component={AboutUs} />
+            <Route path="/AboutUs" component={AboutUs} />
             <Route path="/flexstart" render={ () => <Flexstart removeHeaderFooter={this.removeHeaderFooter}/> }/>
             <Redirect to="/"/>
           </Switch>
---
+++ app/Components/AboutUs.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import ClassScheduleInfo from "./Blocks/ClassScheduleInfo";
+import ClassBanner from "./Blocks/ClassBanner";
+import OurFrontEndProgram from "./Blocks/OurFrontEndProgram";
+import MichaelQuote from "./Blocks/MichaelQuote";
+import SuccessStories from "./Blocks/SuccessStories";
+import NeedMoreInfo from "./Blocks/NeedMoreInfo";
+
+export default class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Flexstart">
+        <ClassBanner />
+        <div className="spacer-sm"></div>
+        <OurFrontEndProgram />
+        <MichaelQuote />
+        <div className="spacer-md"></div>
+        <ClassScheduleInfo />
+        <div className="spacer-md"></div>
+        <SuccessStories />
+        <div className="spacer-sm"></div>
+        <NeedMoreInfo />
+        <div className="spacer-lg"></div>
+      </div>
+    );
+  }
+}
