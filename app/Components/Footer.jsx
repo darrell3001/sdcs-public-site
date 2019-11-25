59d1f454913e70d27ed37df6a3874b878b6be610
unknown <josiahjswab@gmail.com>
2019-11-25 01:25:18 -0800

    console.log ascii art

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import acii from './Blocks/ascii';
 
 export default class Footer extends Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount() {
+    acii();
+  }
+
   render() {
     return (
-
       <div className="footer" style={{ display: this.props.display }}>
         <div className="footer-top">
           <div className="footer-link">
